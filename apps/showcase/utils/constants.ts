49c0cb5eb112927090722280444612eacea7f629
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2026-07-10 16:24:29 +0200

    feat(docs): add global constants (#1295)
    * feat(docs): add global constants

---
+++ apps/showcase/utils/constants.ts
@@ -0,0 +1,6 @@
+export const PROJECT_NAME = 'Optimus UI';
+export const DISCORD_URL = 'https://discord.gg/angular';
+export const GITHUB_REPO_URL = 'https://github.com/openng-org/optimus-ui';
+export const GITHUB_REPO_HEAD = `${GITHUB_REPO_URL}/tree/main`;
+export const GITHUB_DISCUSSIONS_URL = `${GITHUB_REPO_URL}/discussions`;
+export const LINKEDIN_URL = 'https://www.linkedin.com/company/openng';
