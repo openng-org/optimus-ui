7b71ae8478b5667014879a785d08c0bba4ca2ea0
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2026-08-19 00:28:16 +0200

    feat(docs): add update guide

--- apps/docs/pages/migration/routes.ts
+++ apps/docs/pages/migration/routes.ts
@@ -1,8 +1,13 @@
 import { MigrationPrimengDemo } from './primeng';
+import { MigrationUpdateDemo } from './update';
 
 export default [
     {
         path: 'primeng',
         component: MigrationPrimengDemo
+    },
+    {
+        path: 'update',
+        component: MigrationUpdateDemo
     }
 ];
